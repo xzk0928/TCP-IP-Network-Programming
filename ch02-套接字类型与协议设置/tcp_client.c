41d260dfa9e956adb1a20180f4e3e506d4df6efc
xzk0928 <1114001148@qq.com>
2026-01-25 11:59:58 +0000

    2026年1月25日 19:59

--- ch02-套接字类型与协议设置/tcp_client.c
+++ ch02-套接字类型与协议设置/tcp_client.c
@@ -3,27 +3,25 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 
 void error_handling(char *message);
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     int sock;
     struct sockaddr_in serv_addr;
     char message[30];
-    int str_len = 0;
-    int idx = 0, read_len = 0;
+    int str_len;
+    int i; // 用于循环
 
-    if (argc != 3)
-    {
-        printf("Usage: %s <IP> <port>", argv[0]);
+    if(argc != 3) {
+        printf("Usage: %s <IP> <port>\n", argv[0]);
         exit(1);
     }
 
     sock = socket(PF_INET, SOCK_STREAM, 0);
-    if (sock == -1)
+    if(sock == -1)
         error_handling("socket() error");
 
     memset(&serv_addr, 0, sizeof(serv_addr));
@@ -31,41 +29,27 @@ int main(int argc, char *argv[])
     serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_addr.sin_port = htons(atoi(argv[2]));
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
         error_handling("connect() error");
-    // 在 connect 成功之后加入
-    int i;
-    for (i = 0; i < 3000; i++)
-    {
+
+    // 【实验核心】：忙等待，拖慢客户端速度
+    for(i = 0; i < 3000; i++) {
         printf("Wait time %d \n", i);
     }
 
-    // 然后把原本那个逐字节读取的 while 循环改掉
-    // 改成只调用一次 read
+    // 【实验核心】：只调用 1 次 read，尝试读取所有缓冲区数据
     str_len = read(sock, message, sizeof(message) - 1);
-    if (str_len == -1)
+    if(str_len == -1)
         error_handling("read() error!");
 
     printf("Message from server: %s \n", message);
-    printf("Function read call count: 1 \n"); // 此时只调用了1次
+    printf("Function read call count: 1 \n"); // 验证 TCP 无数据边界
 
-    // while(read_len = read(sock, &message[idx++], 1))
-    // {
-    //     if(read_len == -1)
-    //         error_handling("read() error");
-
-    //     str_len += read_len;
-    // }
-
-    // printf("Message from server: %s\n", message);
-    // printf("Function read call count: %d\n", str_len);
     close(sock);
-
     return 0;
 }
 
-void error_handling(char *message)
-{
+void error_handling(char *message) {
     fputs(message, stderr);
     fputc('\n', stderr);
     exit(1);
--- ch02-套接字类型与协议设置/tcp_server.c
+++ ch02-套接字类型与协议设置/tcp_server.c
@@ -3,59 +3,52 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 
 void error_handling(char *message);
 
 int main(int argc, char *argv[])
 {
-    int serv_sock;
-    int clnt_sock;
-
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in clnt_addr;
+    int serv_sock, clnt_sock;
+    struct sockaddr_in serv_addr, clnt_addr;
     socklen_t clnt_addr_sz;
 
-    char message[] = "Hello World\n";
-    if (argc != 2)
-    {
-        printf("Usage: %s <port>", argv[0]);
+    if(argc != 2) {
+        printf("Usage: %s <port>\n", argv[0]);
         exit(1);
     }
 
     serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-    if (serv_sock == -1)
+    if(serv_sock == -1)
         error_handling("socket() error!");
+
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(atoi(argv[1]));
 
-    if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+    if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
         error_handling("bind() error!");
 
-    if (listen(serv_sock, 5) == -1)
+    if(listen(serv_sock, 5) == -1)
         error_handling("listen() error");
 
     clnt_addr_sz = sizeof(clnt_addr);
-    clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_sz);
-    if (clnt_sock == -1)
+    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_sz);
+    if(clnt_sock == -1)
         error_handling("accept() error!");
 
-    // write(clnt_sock, message, sizeof(message));
-    // 在 tcp_server.c 中，把原本的一句 write 改成多句
+    // 【实验核心】：连续发送多次，测试客户端是否能一次接收
     write(clnt_sock, "Hello ", 6);
     write(clnt_sock, "World ", 6);
     write(clnt_sock, "TCP! ", 5);
-    close(serv_sock);
-    close(clnt_sock);
 
+    close(clnt_sock);
+    close(serv_sock);
     return 0;
 }
 
-void error_handling(char *message)
-{
+void error_handling(char *message) {
     fputs(message, stderr);
     fputc('\n', stderr);
     exit(1);
