5afa2a011deed3caf0490e896eed0d5711d74b1d
xzk0928 <1114001148@qq.com>
2026-01-28 07:54:00 +0000

    2026年1月28日 15:54

--- ch04-基于TCP的服务器端和客户端_1/complete_server.c
+++ ch04-基于TCP的服务器端和客户端_1/complete_server.c
@@ -7,6 +7,38 @@
 
 void error_handing(char *message);
 
-int main(int argc,char *argv[]){
+int main(int argc,char *argv[])
+{
+    int serv_sock;
+    int clit_sock;
+
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size;
+
+    char massage[]="Hello World!";
+   
+    if(argc !=2)
+    {
+        printf("Usage :%s <port>\n",argv[0]);
+        exit(1);
+    }
+
+    serv_sock=socket(PF_INET,SOCK_STREAM,0);
+    if(serv_sock==-1)
+    error_handing("socket() error");
+
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+    serv_addr.sin_port=htonl(atoi(argv[1]));
+
+    if(bind(serv_sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr))==-1)
+    error_handing("bind() error");
+
+    if(listen(serv_sock,5)==-1)
+    error_handing("listen() error");
     
+
+
 }
